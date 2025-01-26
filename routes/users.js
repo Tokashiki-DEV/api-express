b338f8b6ea1da863c496eeaaceb91e0327ab4545
Lulks <lulks2202@gmail.com>
2025-01-25 20:51:53 -0400

    add phone route

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,25 @@ import sql from "../db.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send(await sql`SELECT * FROM users.users;`);
+  res.send(
+    await sql`SELECT users.userid, users.firstname, users.lastname, users.email, ARRAY_AGG(phones.phone) AS phones
+              FROM users.users
+              LEFT JOIN users.phones
+              ON users.userid = phones.userid
+              GROUP BY users.userid, users.firstname, users.lastname, users.email`
+  );
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  res.send(await sql`SELECT * FROM users.users WHERE userid LIKE ${id};`);
+  res.send(
+    await sql`SELECT users.userid, users.firstname, users.lastname, users.email, ARRAY_AGG(phones.phone) AS phones
+              FROM users.users
+              LEFT JOIN users.phones
+              ON users.userid = phones.userid
+              WHERE users.userid LIKE ${id}
+              GROUP BY users.userid, users.firstname, users.lastname, users.email`
+  );
 });
 
 router.post("/", async (req, res) => {
@@ -23,6 +36,21 @@ router.post("/", async (req, res) => {
     res.send(`Usuário ${user.firstname} adicionado com sucesso!`);
   } catch {
     res.send("Erro ao cadastrar o usuário");
+  }
+});
+
+router.post("/phone/:id", async (req, res) => {
+  const { phone } = req.body;
+  console.log(phone);
+  const { id } = req.params;
+
+  try {
+    await sql`INSERT INTO users.phones (userid, phone)
+    VALUES(${id}, ${phone});`;
+
+    res.send(`Telefone: ${phone} foi adicionado com sucesso!`);
+  } catch {
+    res.send("Erro ao cadastrar o telefone");
   }
 });
 
